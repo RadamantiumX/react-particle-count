5f21459f11096aa7ea5053e633325814b71c5b7f
RadamantiumX <eduardo2502@gmail.com>
2023-04-03 22:59:46 -0300

    Mejora de la binarizacion

--- src/components/ImageComponent.jsx
+++ src/components/ImageComponent.jsx
@@ -49,16 +49,17 @@ export default function ImageComponent() {
         cv.cvtColor(mat, mat, cv.COLOR_BGR2GRAY);
         cv.threshold(mat, mat, num, 255, cv.THRESH_BINARY);
         
-        cv.findContours(mat, contours, hierarchy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        cv.findContours(mat, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE)
 
         cv.cvtColor(mat, mat, cv.COLOR_GRAY2RGB)
         
         for (let i = 0; i < contours.size(); i++) {
-            const { m00, m01, m10 } = cv.moments(contours.get(i));
+            cv.drawContours(mat, contours, i,[255,0,0,255],1,cv.LINE_8,hierarchy,0);
+           /* const { m00, m01, m10 } = cv.moments(contours.get(i));
             if (m00 === 0) continue;
             const center = new cv.Point(m10 / m00, m01 / m00);
             cv.circle(mat, center, 2, [0, 0, 0, 255], 6);
-            cv.circle(mat, center, 2, [0, 255, 0, 255], 2);
+            cv.circle(mat, center, 2, [0, 255, 0, 255], 2);*/
         }
 
         console.log(contours.size());
